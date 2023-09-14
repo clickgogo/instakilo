8c935ca4ce126407db65597218b051820d0e011d
clickgogo1 <clickgogo1@gmail.com>
2023-09-13 21:17:31 -0300

    refact: change path envs and add in docker

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
-  await app.listen(3300);
+  await app.listen(process.env.PORT);
 }
+
 bootstrap();
